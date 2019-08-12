3a81a2d002714e822b91c123f989fa7f9c5bae3c
Guillaume Desvé <dev@gdesve.dev>
2019-08-12 11:03:19 +0200

    Initial addon stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,174 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import { Table, Grid, Button, Input } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+const Store = window.require("electron-store");
+const addonStore = new Store();
+
+// const fs = window.require("fs");
+
+// onClick={() =>
+//     fs.readdir("/tmp/addons", (err, files) => {
+//         if (err) {
+//             console.log("Unable to scan directory: " + err);
+//         }
+//         files.forEach(file => console.log(file));
+//     })
+// }
+
+const Addon = props => {
+    const [editing, setEditing] = useState(props.editing || false);
+    const [loading, setLoading] = useState(false);
+    const refName = useRef(props.name);
+    const refURL = useRef(props.url);
+    const refVersion = useRef(props.version);
+
+    const validate = editing => {
+        setEditing(false);
+        setLoading(true);
+        fetch(refURL.current)
+            .then(response => response.text())
+            // Get name
+            .then(html => {
+                const results = html.match(/<title>(?<name>[^:]*) .*<\/title>/);
+                refName.current = results.groups.name.trim();
+                return html;
+            })
+            // Get version
+            .then(html => {
+                const results = html.match(
+                    /<div id="version">Version: (?<version>[\w.]+)<\/div>/
+                );
+                refVersion.current = results.groups.version;
+                return html;
+            })
+            .then(() => setLoading(false))
+            .then(() =>
+                props.addAddon({
+                    name: refName.current,
+                    url: refURL.current,
+                    version: refVersion.current
+                })
+            )
+            .catch(err => {
+                console.log(err);
+                setLoading(false);
+            });
+    };
+
+    const urlField = editing ? (
+        <Input
+            fluid
+            placeholder="URL"
+            defaultValue={refURL.current}
+            onChange={(e, data) => (refURL.current = data.value)}
+        />
+    ) : (
+        <a href={refURL.current} target="_blank" rel="noopener noreferrer">
+            {refURL.current}
         </a>
-      </header>
-    </div>
-  );
-}
+    );
+
+    return (
+        <Table.Row>
+            <Table.Cell collapsing>
+                <Button
+                    color="red"
+                    icon="trash alternate"
+                    onClick={() => props.removeAddon(refName.current)}
+                />
+            </Table.Cell>
+            <Table.Cell collapsing>{refName.current}</Table.Cell>
+            <Table.Cell>{urlField}</Table.Cell>
+            <Table.Cell collapsing textAlign="center">
+                {refVersion.current}
+            </Table.Cell>
+            <Table.Cell collapsing>
+                <Button
+                    color={editing ? "green" : "blue"}
+                    icon={editing ? "check" : "edit outline"}
+                    loading={loading}
+                    disabled={loading}
+                    onClick={() => (editing ? validate() : setEditing(true))}
+                />
+            </Table.Cell>
+        </Table.Row>
+    );
+};
+
+const App = () => {
+    const [addons, setAddons] = useState([]);
+
+    addonStore.onDidAnyChange((newValue, oldValue) => {
+        setAddons(Object.values(newValue));
+    });
+
+    const addAddon = addon => {
+        addonStore.set(addon.name, addon);
+    };
+
+    const removeAddon = addonName => {
+        addonStore.delete(addonName);
+    };
+
+    useEffect(() => {
+        setAddons(Object.values(addonStore.store));
+    }, []);
+
+    return (
+        <Grid centered style={{ marginTop: "5vh" }}>
+            <Grid.Column width={14}>
+                <Table selectable celled>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell />
+                            <Table.HeaderCell collapsing>Name</Table.HeaderCell>
+                            <Table.HeaderCell>URL</Table.HeaderCell>
+                            <Table.HeaderCell collapsing>
+                                Version
+                            </Table.HeaderCell>
+                            <Table.HeaderCell />
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {addons.map((addon, i) => (
+                            <Addon
+                                key={addon.name || "unknown"}
+                                {...addon}
+                                addAddon={addAddon}
+                                removeAddon={removeAddon}
+                            />
+                        ))}
+                    </Table.Body>
+                    <Table.Footer>
+                        <Table.Row>
+                            <Table.HeaderCell colSpan={10}>
+                                <Button
+                                    floated="right"
+                                    color="red"
+                                    icon="delete"
+                                    onClick={() => {
+                                        addonStore.clear();
+                                        setAddons([]);
+                                    }}
+                                />
+                                <Button
+                                    floated="right"
+                                    color="blue"
+                                    icon="plus"
+                                    onClick={() => {
+                                        setAddons([
+                                            ...addons,
+                                            { editing: true }
+                                        ]);
+                                    }}
+                                />
+                            </Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Footer>
+                </Table>
+            </Grid.Column>
+        </Grid>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "semantic-ui-css/semantic.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const shell = window.require("electron").shell;
+
+// Open links externally by default
+document.addEventListener("click", event => {
+	if (event.target.tagName === "a" && event.target.href.startsWith("http")) {
+		event.preventDefault();
+		shell.openExternal(event.target.href);
+	}
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
