ad940206c2d8368406d421e4dad18582828aff0a
Guillaume Desvé <dev@gdesve.dev>
2019-08-20 12:05:42 +0200

    Add Folder select dialog

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
-import React, { useRef } from "react";
-import { Grid, Input, Tab } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Grid, Input, Tab, Button } from "semantic-ui-react";
 
 import { AddonStore } from "./Store";
 import { CFTab } from "./CurseForge/CurseForge";
 import { WITab } from "./WowInterface/WowInterface";
 // import { TukuiTab } from "./Tukui/Tukui";
 
+const { dialog } = window.require("electron").remote;
+
 const App = () => {
-    const refPathTimeout = useRef(null);
+    const [pathValue, setPathValue] = useState(AddonStore.get("path", ""));
 
-    const updatePath = path => {
-        clearTimeout(refPathTimeout.current);
-        refPathTimeout.current = setTimeout(() => {
-            AddonStore.set("path", path);
-            refPathTimeout.current = null;
-        }, 500);
-    };
-
-    const panes = [
-        { menuItem: "Curse Forge (classic)", pane: <CFTab key="curseforge" /> },
-        { menuItem: "WoW Interface", pane: <WITab key="wowinterface" /> }
-        // { menuItem: "Tukui", pane: <TukuiTab key="tukui" /> }
-    ];
+    // Hide tabs until a folder is selected
+    let panes = [];
+    if (pathValue !== "")
+        panes = [
+            {
+                menuItem: "Curse Forge (classic)",
+                pane: <CFTab key="curseforge" />
+            },
+            { menuItem: "WoW Interface", pane: <WITab key="wowinterface" /> }
+            // { menuItem: "Tukui", pane: <TukuiTab key="tukui" /> }
+        ];
 
     return (
         <Grid centered style={{ marginTop: "2vh" }}>
             <Grid.Column width={14}>
                 <Grid.Row>
-                    <Input
-                        fluid
-                        defaultValue={AddonStore.get("path")}
-                        placeholder="Path to WoW addons folder"
-                        onChange={(e, { value }) => updatePath(value)}
-                    />
+                    <Grid>
+                        <Grid.Column width={15}>
+                            <Input
+                                fluid
+                                disabled
+                                value={pathValue}
+                                placeholder="Path to WoW addons folder"
+                            />
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Button
+                                color={pathValue ? "" : "red"}
+                                icon="folder"
+                                onClick={() => {
+                                    const path = dialog.showOpenDialogSync({
+                                        properties: ["openDirectory"]
+                                    });
+                                    if (path) {
+                                        AddonStore.set("path", path[0]);
+                                        setPathValue(path[0]);
+                                    }
+                                }}
+                            />
+                        </Grid.Column>
+                    </Grid>
                 </Grid.Row>
                 <Grid.Row style={{ marginTop: "2vh" }}>
                     <Tab
